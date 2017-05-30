1641f7f80280f4e7c3849b73cadf350c54acc5ef
Rohit <rohhittt@gmail.com>
2017-05-30 16:09:29 +0530

    Adding express register helper

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Jist = require('./jist');
+const fs = require('fs');
 
 module.exports = {
   convert: (input, strategy) => {
@@ -6,5 +7,22 @@ module.exports = {
     strategy.apply(input, [jist]);
 
     return jist.output;
+  },
+
+  register: function(expressApp){
+    let self = this;
+
+    expressApp.engine('jist', function (filePath, scope, callback) {
+      fs.readFile(filePath, function (err, content) {
+        if (err) return callback(err);
+        let renderer = Function('jist', content);
+
+        let rendered = self.convert(scope, renderer);
+
+        return callback(null, rendered)
+      })
+    });
+
+    expressApp.set('view engine', 'jist');
   }
 };
