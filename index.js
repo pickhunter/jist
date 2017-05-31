727b4b2719c95990a16bf33bffd7e851a82fef19
Rohit <rohhittt@gmail.com>
2017-05-31 12:29:25 +0530

    Going the JS route

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const Jist = require('./jist');
 const fs = require('fs');
+const _ = require('lodash');
 
 let jistModule = {
-  convert: (input, strategy) => {
-    let jist = new Jist(input);
-    strategy.apply(input, [jist]);
+  convert: (scope, template) => {
+    let scopeKeys = _.keys(scope);
 
-    return jist.output;
+    let renderer = Function(...scopeKeys , template).bind(scope);
+
+    let rendered = renderer(...scopeKeys.map(key => scope[key]));
+
+    return rendered;
+  },
+
+  render: function() {
+    return this.convert(...arguments);
   },
 
   register: function(expressApp){
@@ -20,12 +28,24 @@ let jistModule = {
 jistModule.renderFile = function(filePath, scope, callback) {
   fs.readFile(filePath, (err, content) => {
     if (err) return callback(err);
-    let renderer = Function('jist', content);
 
-    let rendered = jistModule.convert(scope, renderer);
+    // scope = _.omit(scope, ['cache', '_locals', 'settings']);
+    let scopeKeys = _.keys(scope);
+
+    let renderer = Function(...scopeKeys , content).bind(scope);
+
+    let rendered = renderer(...scopeKeys.map(key => scope[key]));
 
     return callback(null, rendered);
   });
+};
+
+jistModule.getFileRenderer = function(options) {
+  let defaultScope = options.defaultScope || {};
+
+  return function(filePath, scope, callback) {
+    return jistModule.renderFile(filePath, Object.assign(defaultScope, scope), callback);
+  }
 };
 
 jistModule._express = jistModule.renderFile;
